062c79a4a111a1f5621b8fe7eeb9a07f606e9145
Leo Horie <lhorie@uber.com>
2018-01-17 12:36:46 -0800

    Migrate to fusion-core 0.3.0-2 (DI API)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import browserDataFetching from './browser.js';
-import serverDataFetching from './server.js';
+import browserDataFetching from './browser';
+import serverDataFetching from './server';
 export {default as mock} from './mock';
 
 const RPC = __BROWSER__ ? browserDataFetching : serverDataFetching;
 
 export default RPC;
+export {RPCHandlersToken} from './tokens';
+export {RPCRoutePrefixConfigToken} from './browser';
--- src/mock.js
+++ src/mock.js
@@ -1,15 +1,18 @@
 import {Plugin} from 'fusion-core';
 import MissingHandlerError from './missing-handler-error';
+import {withDependencies} from 'fusion-core';
+import {RPCHandlersToken} from './tokens';
 
-export default ({handlers} = {}) => {
-  return new Plugin({
-    Service: class RPC {
-      async request(method, args) {
-        if (!handlers[method]) {
-          throw new MissingHandlerError(method);
-        }
-        return handlers[method](args);
+export default withDependencies({
+  handlers: RPCHandlersToken
+})(({handlers} = {}) => {
+  class RPC {
+    async request(method, args) {
+      if (!handlers[method]) {
+        throw new MissingHandlerError(method);
       }
-    },
-  });
-};
+      return handlers[method](args);
+    }
+  }
+  return () => new RPC();
+});

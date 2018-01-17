0da1bdf6d74f7e83403e2a8dc6a59ac635e1a8e2
Leo Horie <lhorie@uber.com>
2018-01-17 13:23:07 -0800

    expose rpctoken, update docs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {createToken} from 'fusion-tokens';
 import browserDataFetching from './browser';
 import serverDataFetching from './server';
 
@@ -5,6 +6,8 @@ export {default as mock} from './mock';
 
 const RPC = __BROWSER__ ? browserDataFetching : serverDataFetching;
 
+
 export default RPC;
+export const RPCToken = createToken('RPCToken');
 export {RPCHandlersToken} from './tokens';
 export {RPCRoutePrefixConfigToken} from './browser';

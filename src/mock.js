de7c17780182553110b4a489a902da1a70e58d78
Alexander Smith <amsmith@uber.com>
2018-05-09 15:47:00 -0700

    Improve Flow coverage for source files

--- src/mock.js
+++ src/mock.js
@@ -2,23 +2,36 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
  */
 
-// @flow
-
 import {createPlugin} from 'fusion-core';
-import type {FusionPlugin} from 'fusion-core';
+import type {Context} from 'fusion-core';
+import {UniversalEventsToken} from 'fusion-plugin-universal-events';
+
 import MissingHandlerError from './missing-handler-error';
 import {RPCHandlersToken} from './tokens';
+import type {HandlerType} from './tokens.js';
+import type {RPCPluginType} from './types.js';
+
+type ExtractReturnType = <V>(() => V) => V;
 
 class RPC {
-  handlers: *;
+  ctx: ?Context;
+  emitter: ?$Call<ExtractReturnType, typeof UniversalEventsToken>;
+  handlers: ?HandlerType;
+  fetch: ?*;
 
   constructor(handlers: any) {
     this.handlers = handlers;
   }
 
-  async request(method, args) {
+  async request(method: string, args: mixed) {
+    if (!this.handlers) {
+      throw new Error('fusion-plugin-rpc requires `handlers`');
+    }
+
     if (!this.handlers[method]) {
       throw new MissingHandlerError(method);
     }
@@ -26,8 +39,6 @@ class RPC {
   }
 }
 
-type RPCServiceFactory = {from: () => RPC};
-type RPCPluginType = FusionPlugin<*, RPCServiceFactory>;
 const plugin: RPCPluginType = createPlugin({
   deps: {
     handlers: RPCHandlersToken,
